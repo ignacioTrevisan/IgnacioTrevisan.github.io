233a216219228291cc10ba35f614a1b9e42b5bb7
Ignacio Trevisan <106756210+ignacioTrevisan@users.noreply.github.com>
2023-07-26 20:52:24 -0300

    main.js

--- main.js
+++ main.js
@@ -15,22 +15,3 @@ function transparencia()
         requestAnimationFrame(transparencia);
    }
 transparencia();
-var imagenes = 
-[
-"imagenes/proyectoEnAndroid/1.jpeg",
-"imagenes/proyectoEnAndroid/2.jpeg",
-"imagenes/proyectoEnAndroid/3.jpeg",
-"imagenes/proyectoEnAndroid/4.jpeg"
-];
-var index=0;
-function cambiarImagenes()
-{
-     var imagenCambiante = document.getElementById("imagen-cambiante");
-     imagenCambiante.style.backgroundImage ="url('" + imagenes[index] + "')";
-     index++;
-if(index>=imagenes.length)
-{
-     index = 0;
-}
-}
-setInterval(cambiarImagenes, 3000);
